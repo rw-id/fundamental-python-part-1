9c768c48a8ad6ec75771b90401ed1cdeb4da1663
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2020-07-14 22:24:27 +0800

    Sintak Dasar -- Perulangan

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-# KONSTRUKSI DASAR PYTHON
-# SEQUENTIAL = Eksekusi kode berurutan
+# Sintaks DASAR PYTHON
+# 1. SEQUENTIAL = Eksekusi kode berurutan
+
 print('Hello World!')
 print('by Riffadi')
 print('Tanggal 14 Juli 2020')
 print('-' * 20)
+
+# 2. Percabangan(branching)  = eksekusi terpilih
+# Membuat data / variabel
+
+data = False
+
+if data:
+ print('Done')
+else:
+ print('NOT COMPLETE')
